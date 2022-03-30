94a704c68603f23c4689e873b707ac9f82d16286
藤村　翔吾 <admin@Mac-mini.local>
2022-03-30 15:35:02 +0900

    20220330アップ

--- script/script.js
+++ script/script.js
@@ -1,13 +1,35 @@
 jQuery( function( $ ) {
+  var win = $(window).width();
+  var tablet = 834; // ブレークポイント（px）タブレットサイズ
+  var pc = 1920; // ブレークポイント（px）　PCサイズ
     $( ".js-menu" ).on( "click", function() {
-      if ($(".p-gmenu").hasClass("is-open")) {
-        $(".p-gmenu").removeClass("is-open");
+      if ($(".p-gmenu").hasClass("p-gmenu__is-open")) {
         $( ".c-button-menu" ).removeClass( "c-button-menu__is-open" );
-        $( ".p-gmenu" ).fadeOut(300);}
+        $(".p-gmenu").removeClass("p-gmenu__is-open");
+      }
       else {
-        $( ".p-gmenu" ).addClass( "is-open" );
         $( ".c-button-menu" ).addClass( "c-button-menu__is-open" );
-        $( ".p-gmenu" ).fadeIn(300);
+        $( ".p-gmenu" ).addClass( "p-gmenu__is-open" );
       } 
     } );
 } );
+
+/* imgタグ変更用　一旦不要なのでコメントアウト
+$(window).on('load resize',function () {                     // Windowサイズが変更された時
+  var win = $(window).width();
+  var tablet = 834; // ブレークポイント（px）タブレットサイズ
+  var pc = 1920; // ブレークポイント（px）　PCサイズ
+  if (win < tablet) {
+    $('.p-article-top').children('img').attr('src', './img/phone_main.jpg');
+    $('.p-article-map').children('img').attr('src', './img/phone_map.jpg');
+  }
+  if (win > tablet && win < pc) {
+    $('.p-article-top').children('img').attr('src', './img/tablet_main.jpg');
+    $('.p-article-map').children('img').attr('src', './img/tablet_map.jpg');
+  }
+  if (win > pc) {
+    $('.p-article-top').children('img').attr('src', './img/pc_main.jpg');
+    $('.p-article-map').children('img').attr('src', './img/pc_map.jpg');
+  }
+});
+*/
